a6d342f7dfc6be981ddbb038931c5bd9077fe04d
Alexander Matskevich <a.matskevich@studionx.ru>
2021-07-21 10:33:05 +0300

    Added all tutors.

---
+++ 2_interfaces.js
@@ -0,0 +1,16 @@
+var rect1 = {
+    id: '1234',
+    size: {
+        width: 20,
+        height: 30
+    },
+    color: '#ccc'
+};
+var rect2 = {
+    id: '12345',
+    size: {
+        width: 10,
+        height: 5
+    }
+};
+rect2.color = 'black';
